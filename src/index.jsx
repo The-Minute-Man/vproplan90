2cd6e42b9b17959b8b07fa590dc4e0129343f703
The-Minute-Man <samuel.joseph@live.com>
2025-03-19 02:25:06 -0700

    Add files via upload

---
+++ src/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import Planner from './Planner.jsx';
+import KTestr from './ktestr'
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
+import ActivitySetter from './ActivitySetter.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>
+  },
+  {
+    path: "/planner",
+    element: <Planner/>
+  },
+  {
+    path: "/ktestr",
+    element: <KTestr/>
+  },
+  {
+    path: "/activitysetter",
+    element: <ActivitySetter/>
+  }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
